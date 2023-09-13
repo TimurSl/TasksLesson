489f1f1ef29b9158010143149210be40f47f1ce7
Tymur Slynko <timurslinko1@gmail.com>
2023-09-13 18:55:30 +0300

    Add Bolean class with various methods and related unit tests
    This commit introduces a new Boolean class in the TasksLesson/Homework. The class is composed of several static methods which calculate and return boolean results based on given parameters. These methods cover various logic operations like comparatives, odd or even checks, positive or negative checks, and other specific calculations for different cases.
    Additionly, a series of unit tests were added for each method of the Boolean class, to ensure the proper

---
+++ TasksLesson/Homework/Boolean.cs
@@ -0,0 +1,227 @@
+﻿namespace TasksLesson.Homework;
+
+public class Boolean
+{
+	public static bool Boolean1(int a)
+    {
+        return a > 0;
+    }
+
+    public static bool Boolean2(int a)
+    {
+        return a % 2 != 0;
+    }
+
+    public static bool Boolean3(int a)
+    {
+        return a % 2 == 0;
+    }
+
+    public static bool Boolean4(int a, int b)
+    {
+        return a > 2 && b <= 3;
+    }
+
+    public static bool Boolean5(int a, int b)
+    {
+        return a >= 0 || b < -2;
+    }
+
+    public static bool Boolean6(int a, int b, int c)
+    {
+        return a < b && b < c;
+    }
+
+    public static bool Boolean7(int a, int b, int c)
+    {
+        return a < b && b < c;
+    }
+
+    public static bool Boolean8(int a, int b)
+    {
+        return a.IsOdd () && b.IsOdd ();
+    }
+
+    public static bool Boolean9(int a, int b)
+    {
+        return a.IsOdd () || b.IsOdd ();
+    }
+
+    public static bool Boolean10(int a, int b)
+    {
+        return a.IsOdd () ^ b.IsOdd ();
+    }
+
+    public static bool Boolean11(int a, int b)
+    {
+        return (a.IsOdd () && b.IsOdd ()) || (a.IsEven () && b.IsEven ());
+    }
+
+    public static bool Boolean12(int a, int b, int c)
+    {
+        return a.IsPositive () && b.IsPositive () && c.IsPositive ();
+    }
+
+    public static bool Boolean13(int a, int b, int c)
+    {
+        return a.IsPositive () || b.IsPositive () || c.IsPositive ();
+    }
+
+    public static bool Boolean14(int a, int b, int c)
+    {
+        return a.IsPositive () ^ b.IsPositive () ^ c.IsPositive ();
+    }
+
+    public static bool Boolean15(int a, int b, int c)
+    {
+        return (a.IsPositive () && b.IsPositive ())
+               ^ (a.IsPositive () && c.IsPositive ())
+               ^ (b.IsPositive () && c.IsPositive ());
+    }
+
+    public static bool Boolean16(int a)
+    {
+        return a < 100 && a > 9 && a.IsEven ();
+    }
+
+    public static bool Boolean17(int a)
+    {
+        return a < 1000 && a > 99 && a % 2 != 0;
+    }
+
+    public static bool Boolean18(int a, int b, int c)
+    {
+        return (a == b) || (a == c) || (b == c);
+    }
+
+    public static bool Boolean19(int a, int b, int c)
+    {
+        return (a > 0 && b < 0) || (a < 0 && b > 0) // a and b
+            || (a > 0 && c < 0) || (a < 0 && c > 0) // a and c
+            || (b > 0 && c < 0) || (b < 0 && c > 0); // b and c
+    }
+
+    public static bool Boolean20(int abc)
+    {
+        int a = abc / 100;
+        int b = abc / 10 % 10;
+        int c = abc % 10;
+        return a != b && a != c && b != c;
+    }
+
+    public static bool Boolean21(int abc)
+    {
+        int a = abc / 100;
+        int b = abc / 10 % 10;
+        int c = abc % 10;
+        return a < b && b < c;
+    }
+
+    public static bool Boolean22(int abc)
+    {
+        int a = abc / 100;
+        int b = abc / 10 % 10;
+        int c = abc % 10;
+        return (a < b && b < c) || (c < b && b < a);
+    }
+
+    public static bool Boolean23(int abcd)
+    {
+        int a = abcd / 1000;
+        int b = abcd / 100 % 10;
+        int c = abcd / 10 % 10;
+        int d = abcd % 10;
+        return a == d && b == c;
+    }
+
+    public static bool Boolean24(int a, int b, int c)
+    {
+        int dicriminant = (b * b) - (4 * a * c);
+        return dicriminant >= 0;
+    }
+
+    public static bool Boolean25(int x, int y)
+    {
+        return x < 0 && y > 0;
+    }
+
+    public static bool Boolean26(int x, int y)
+    {
+        return x > 0 && y < 0;
+    }
+
+    public static bool Boolean27(int x, int y)
+    {
+        return (x < 0 && y > 0) || (x > 0 && y > 0);
+    }
+
+    public static bool Boolean28(int x, int y)
+    {
+        return (x > 0 && y > 0) || (x < 0 && y < 0);
+    }
+
+    public static bool Boolean29(int x, int y, int x1, int y1, int x2, int y2)
+    {
+        Point topLeft = new Point(x1, y1);
+        Point bottomRight = new Point(x2, y2);
+        Point point = new Point(x, y);
+
+        return point.IsInside(topLeft, bottomRight);
+    }
+
+    public static bool Boolean30(int a, int b, int c)
+    {
+        return (a == b) && (c == b) && (c == a);
+    }
+
+    public static bool Boolean31(int a, int b, int c)
+    {
+        return (a == b) || (c == b) || (c == a);
+    }
+
+    public static bool Boolean32(int a, int b, int c)
+    {
+        return (a == b + c) || (c == b + a) || (b == c + a);
+    }
+
+    public static bool Boolean33(int a, int b, int c)
+    {
+        return (a + b) > c && (a + c) > b && (b + c) > a;
+    }
+
+    public static bool Boolean34(int x, int y)
+    {
+        return (x + y).IsOdd ();
+    }
+
+    public static bool Boolean35(int x1, int y1, int x2, int y2)
+    {
+        return (x1 + y1) % 2 == (x2 + y2) % 2;
+    }
+
+    public static bool Boolean36(int x1, int y1, int x2, int y2)
+    {
+        return (x1 == x2) || (y1 == y2);
+    }
+
+    public static bool Boolean37(int x1, int y1, int x2, int y2)
+    {
+        return Math.Abs(x1 - x2) <= 1 && Math.Abs(y1 - y2) <= 1;
+    }
+
+    public static bool Boolean38(int x1, int y1, int x2, int y2)
+    {
+        return Math.Abs(x1 - x2) == Math.Abs(y1 - y2);
+    }
+
+    public static bool Boolean39(int x1, int y1, int x2, int y2)
+    {
+        return Boolean37(x1, y1, x2, y2) || Boolean38(x1, y1, x2, y2);
+    }
+
+    public static bool Boolean40(int x1, int y1, int x2, int y2)
+    {
+        return Math.Abs(x1 - x2) == 2 && Math.Abs(y1 - y2) == 1
+               || Math.Abs(x1 - x2) == 1 && Math.Abs(y1 - y2) == 2;
+    }
+}
---
+++ Tests/Boolean.cs
@@ -0,0 +1,330 @@
+﻿namespace Tests;
+
+public class Boolean
+{
+	[SetUp]
+	public void Setup()
+	{
+	}
+
+	[Test]
+	public void Boolean1()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean1(1));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean1(0));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean1(-1));
+	}
+
+	[Test]
+	public void Boolean2()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean2(1));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean2(0));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean2(-1));
+	}
+
+	[Test]
+	public void Boolean3()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean3(2));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean3(1));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean3(0));
+	}
+
+	[Test]
+	public void Boolean4()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean4(3, 2));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean4(2, 3));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean4(2, 2));
+	}
+
+	[Test]
+	public void Boolean5()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean5(0, -3));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean5(-2, -2));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean5(1, -3));
+	}
+
+	[Test]
+	public void Boolean6()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean6(1, 2, 3));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean6(1, 3, 2));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean6(3, 2, 1));
+	}
+
+	[Test]
+	public void Boolean7()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean7(1, 2, 3));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean7(1, 3, 2));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean7(3, 2, 1));
+	}
+
+	[Test]
+	public void Boolean8()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean8(1, 3));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean8(1, 2));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean8(2, 3));
+	}
+
+	[Test]
+	public void Boolean9()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean9(1, 3));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean9(2, 3));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean9(2, 2));
+	}
+
+	[Test]
+	public void Boolean10()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean10(1, 2));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean10(2, 3));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean10(2, 2));
+	}
+
+	[Test]
+	public void Boolean11()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean11(1, 3));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean11(2, 4));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean11(2, 3));
+	}
+
+	[Test]
+	public void Boolean12()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean12(1, 2, 3));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean12(2, 3, 4));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean12(-2, -3, 3));
+	}
+
+	[Test]
+	public void Boolean13()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean13(-1, -2, 3));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean13(-2, 3, 4));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean13(-2, -3, -3));
+	}
+
+	[Test]
+	public void Boolean14()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean14(-1, -2, 3));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean14(-2, 3, -4));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean14(-2, 3, 3));
+	}
+
+	[Test]
+	public void Boolean15()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean15(-1, 2, 3));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean15(2, 3, -4));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean15(-2, -3, 3));
+	}
+
+	[Test]
+	public void Boolean16()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean16(12));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean16(15));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean16(1202));
+	}
+
+	[Test]
+	public void Boolean17()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean17(121));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean17(150));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean17(1202));
+	}
+
+	[Test]
+	public void Boolean18()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean18(3, 3, 2));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean18(3, 2, 3));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean18(-2, 5, 3));
+	}
+
+	[Test]
+	public void Boolean19()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean19(3, -3, 2));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean19(3, 2, -3));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean19(3, 5, 3));
+	}
+
+	[Test]
+	public void Boolean20()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean20(123));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean20(322));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean20(222));
+	}
+
+	[Test]
+	public void Boolean21()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean21(123));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean21(322));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean21(222));
+	}
+
+	[Test]
+	public void Boolean22()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean22(123));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean22(321));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean22(222));
+	}
+
+	[Test]
+	public void Boolean23()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean23(1221));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean23(2002));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean23(2223));
+	}
+
+	[Test]
+	public void Boolean24()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean24(1, 8, 3));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean24(2, 6, 4));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean24(1, 2, 3));
+	}
+
+	[Test]
+	public void Boolean25()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean25(-1, 8));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean25(-2, 6));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean25(1, 2));
+	}
+
+	[Test]
+	public void Boolean26()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean26(1, -8));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean26(2, -6));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean26(1, 2));
+	}
+
+	[Test]
+	public void Boolean27()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean27(3, 8));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean27(-2, 6));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean27(-1, -2));
+	}
+
+	[Test]
+	public void Boolean28()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean28(3, 8));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean28(-2, -6));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean28(-1, 2));
+	}
+
+	[Test]
+	public void Boolean29()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean29(0, 0, 5, 5, -5,-5));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean29(1, 1, 2, 2, -1, -1));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean29(1, 1, 2, 2, 1, 2));
+	}
+
+	[Test]
+	public void Boolean30()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean30(3, 3, 3));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean30(4, 4, 4));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean30(1, 2, 3));
+	}
+
+	[Test]
+	public void Boolean31()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean31(3, 6, 3));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean31(4, 8, 4));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean31(1, 2, 3));
+	}
+
+	[Test]
+	public void Boolean32()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean32(9, 6, 3));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean32(4, 8, 4));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean32(9, 2, 3));
+	}
+
+	[Test]
+	public void Boolean33()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean33(3, 8, 9));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean33(4, 8, 6));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean33(12, 112, 12));
+	}
+
+	[Test]
+	public void Boolean34()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean34(2, 1));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean34(4, 3));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean34(1, 1));
+	}
+
+	[Test]
+	public void Boolean35()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean35(2, 1, 4, 3));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean35(6, 5, 8, 7));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean35(1, 1, 1, 2));
+	}
+
+	[Test]
+	public void Boolean36()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean36(1, 1, 9, 1));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean36(2, 3, 2, 9));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean36(1, 1, 2, 2));
+	}
+
+	[Test]
+	public void Boolean37()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean37(1, 1, 2, 1));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean37(2, 3, 3, 4));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean37(1, 1, 5, 2));
+	}
+
+	[Test]
+	public void Boolean38()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean38(1, 1, 3, 3));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean38(4, 3, 8, 7));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean38(1, 1, 1, 8));
+	}
+
+	[Test]
+	public void Boolean39()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean39(1, 1, 3, 3));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean39(4, 3, 8, 7));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean39(1, 1, 1, 8));
+	}
+
+
+	[Test]
+	public void Boolean40()
+	{
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean40(8, 1, 6, 2));
+		Assert.IsTrue(TasksLesson.Homework.Boolean.Boolean40(4, 7, 5, 5));
+		Assert.IsFalse(TasksLesson.Homework.Boolean.Boolean40(1, 1, 1, 8));
+	}
+}
