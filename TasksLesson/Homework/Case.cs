e4300ce76ced14d9dc640bd4301347c7177383b8
Tymur Slynko <timurslinko1@gmail.com>
2023-09-15 18:12:34 +0300

    Add new test cases and implementation for Russian language tasks
    These changes introduce several new test cases (and implementations) that suit a number of different situations. These include tests for days of the week, Russian numerical values, date and time functions, and Zodiac sign calculation. The implementation has been verified through unit tests..

---
+++ TasksLesson/Homework/Case.cs
@@ -0,0 +1,664 @@
+﻿using System.Text;
+
+namespace TasksLesson.Homework;
+
+public class Case
+{
+	public static string Case1(int a)
+	{
+		switch (a)
+		{
+			case 1:
+				return "Понедельник";
+			case 2:
+				return "Вторник";
+			case 3:
+				return "Среда";
+			case 4:
+				return "Четверг";
+			case 5:
+				return "Пятница";
+			case 6:
+				return "Суббота";
+			case 7:
+				return "Воскресенье";
+			default:
+				return "Нет такого дня недели";
+		}
+	}
+
+	public static string Case2(int k)
+	{
+		switch (k)
+		{
+			case 1:
+				return "Плохо";
+			case 2:
+				return "Неудовлетворительно";
+			case 3:
+				return "Удовлетворительно";
+			case 4:
+				return "Хорошо";
+			case 5:
+				return "Отлично";
+			default:
+				return "Нет такой оценки";
+		}
+	}
+
+	public static string Case3(int a)
+	{
+		switch (a)
+		{
+			case 1:
+				return "Январь";
+
+			case 2:
+				return "Февраль";
+
+			case 3:
+				return "Март";
+
+			case 4:
+				return "Апрель";
+
+			case 5:
+				return "Май";
+
+			case 6:
+				return "Июнь";
+
+			case 7:
+				return "Июль";
+
+			case 8:
+				return "Август";
+
+			case 9:
+				return "Сентябрь";
+
+			case 10:
+				return "Октябрь";
+
+			case 11:
+				return "Ноябрь";
+
+			case 12:
+				return "Декабрь";
+
+			default:
+				return "Нет такого месяца";
+		}
+	}
+
+	public static string Case4(int a)
+	{
+		switch (a)
+		{
+			case 1:
+				return "31";
+			case 2:
+				return "28";
+			case 3:
+				return "31";
+			case 4:
+				return "30";
+			case 5:
+				return "31";
+			case 6:
+				return "30";
+			case 7:
+				return "31";
+			case 8:
+				return "31";
+			case 9:
+				return "30";
+			case 10:
+				return "31";
+			case 11:
+				return "30";
+			case 12:
+				return "31";
+			default:
+				return "Нет такого месяца";
+		}
+	}
+
+	public static string Case5(int a, int b, int c)
+	{
+		switch (c)
+		{
+			case 1:
+				return (a + b).ToString ();
+			case 2:
+				return (a - b).ToString ();
+			case 3:
+				return (a * b).ToString ();
+			case 4:
+				if (b == 0)
+				{
+					return "Нельзя делить на ноль";
+				}
+
+				return (a / b).ToString ();
+			default:
+				return "Нет такого действия";
+		}
+	}
+
+	public static double? Case6(int a, double b)
+	{
+		switch (a)
+		{
+			case 1:
+				return b * 0.1;
+			case 2:
+				return b * 1000;
+			case 3:
+				return b;
+			case 4:
+				return b * 0.001;
+			case 5:
+				return b * 0.01;
+			default:
+				return null;
+		}
+	}
+
+	public static double Case7(int a, double b)
+	{
+		switch (a)
+		{
+			case 1: // Килограммы
+				return b;
+			case 2: // Миллиграмы
+				return b * 0.000001;
+			case 3: // Граммы
+				return b * 0.001;
+			case 4: // Тонны
+				return b * 1000;
+			case 5: // Центнеры
+				return b * 100;
+			default:
+				return -1;
+		}
+	}
+
+	public static string Case8(int a, int b)
+	{
+		switch (a)
+		{
+			case 1:
+				return "31" + " " + "12";
+			case 2:
+				return "31" + " " + "1";
+			case 3:
+				return "28" + " " + "2";
+			case 4:
+				return "31" + " " + "3";
+			case 5:
+				return "30" + " " + "4";
+			case 6:
+				return "31" + " " + "5";
+			case 7:
+				return "30" + " " + "6";
+			case 8:
+				return "31" + " " + "7";
+			case 9:
+				return "31" + " " + "8";
+			case 10:
+				return "30" + " " + "9";
+			case 11:
+				return "31" + " " + "10";
+			case 12:
+				return "30" + " " + "11";
+			default:
+				return "There's no such month";
+		}
+	}
+
+	public static string Case9(int d, int m)
+	{
+		switch (m)
+		{
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+			case 11:
+			case 12:
+				return d + 1 + " " + m;
+			default:
+				return "Нет такого месяца";
+		}
+	}
+
+	public static string Case10(char c, int n)
+	{
+		string result;
+		switch (c)
+		{
+			case 'С':
+				result = n switch
+				{
+					0 => "С",
+					1 => "З",
+					-1 => "В",
+					_ => "Нет такой команды"
+				};
+
+				break;
+			case 'З':
+				result = n switch
+				{
+					0 => "З",
+					1 => "Ю",
+					-1 => "С",
+					_ => "Нет такой команды"
+				};
+
+				break;
+			case 'Ю':
+				result = n switch
+				{
+					0 => "Ю",
+					1 => "В",
+					-1 => "З",
+					_ => "Нет такой команды"
+				};
+
+				break;
+			case 'В':
+				result = n switch
+				{
+					0 => "В",
+					1 => "С",
+					-1 => "Ю",
+					_ => "Нет такой команды"
+				};
+
+				break;
+			default:
+				result = "Нет такого направления";
+				break;
+		}
+
+		return result;
+	}
+
+	public static char Case11(char c, int n1, int n2)
+	{
+		char GetFinalDirection(char direction, int n) => direction switch
+		{
+			'С' => n switch
+			{
+				1 => 'З',
+				-1 => 'В',
+				2 => 'Ю',
+				_ => direction
+			},
+			'З' => n switch
+			{
+				1 => 'Ю',
+				-1 => 'С',
+				2 => 'В',
+				_ => direction
+			},
+			'Ю' => n switch
+			{
+				1 => 'В',
+				-1 => 'З',
+				2 => 'С',
+				_ => direction
+			},
+			'В' => n switch
+			{
+				1 => 'С',
+				-1 => 'Ю',
+				2 => 'З',
+				_ => direction
+			},
+			_ => direction
+		};
+
+		char final_direction = GetFinalDirection(c, n1);
+		return GetFinalDirection(final_direction, n2);
+	}
+
+
+	public static string Case12(int n, double value)
+	{
+		double R = 0;
+		double D = 0;
+		double L = 0;
+		double S = 0;
+		switch (n)
+		{
+			case 1:
+				R = Math.Round(value, 1);
+				D = Math.Round(2 * R, 1);
+				L = Math.Round(2 * Math.PI * R, 1);
+				S = Math.Round(Math.PI * R * R, 1);
+				break;
+			case 2:
+				D = Math.Round(value, 1);
+				R = Math.Round(D / 2, 1);
+				L = Math.Round(2 * Math.PI * R, 1);
+				S = Math.Round(Math.PI * R * R, 1);
+				break;
+			case 3:
+				L = Math.Round(value, 1);
+				R = Math.Round(L / (2 * Math.PI), 1);
+				S = Math.Round(Math.PI * R * R, 1);
+				D = Math.Round(2 * R, 1);
+				break;
+			case 4:
+				S = Math.Round(value, 1);
+				R = Math.Round(Math.Sqrt(S / Math.PI), 1);
+				D = Math.Round(2 * R, 1);
+				L = Math.Round(2 * Math.PI * R, 1);
+				break;
+			default:
+				return "Incorrect input";
+		}
+
+		return $"R = {R}, D = {D}, L = {L}, S = {S}";
+	}
+
+	public static string Case13(int n, double value)
+	{
+		double a, c, h, S;
+
+		(a, c, h, S) = n switch
+		{
+			1 => (
+				Math.Round(value, 2),
+				Math.Round(value * Math.Sqrt(2), 2),
+				Math.Round(value * Math.Sqrt(2) / 2, 2),
+				Math.Round(value * Math.Sqrt(2) / 2 * value / 2, 2)
+			),
+			2 => (
+				Math.Round(value / Math.Sqrt(2), 2),
+				Math.Round(value, 2),
+				Math.Round(value / 2, 2),
+				Math.Round(value / 2 * value / 2, 2)),
+			3 => (
+				Math.Round(value * 2 / Math.Sqrt(2), 2),
+				Math.Round(value * 2, 2),
+				Math.Round(value, 2),
+				Math.Round(value * 2 / 2, 2)
+			),
+			4 => (
+				Math.Round(Math.Sqrt(value * 2) / Math.Sqrt(2), 2),
+				Math.Round(Math.Sqrt(value * 2), 2),
+				Math.Round(Math.Sqrt(value * 2) / 2, 2),
+				Math.Round(value, 2)
+			),
+			_ => (0.0, 0.0, 0.0, 0.0)
+		};
+
+		return $"a = {a}, c = {c}, h = {h}, S = {S}";
+	}
+
+	public static string Case14(int n, double value)
+	{
+		double a = 0;
+		double R1 = 0;
+		double R2 = 0;
+		double S = 0;
+		switch (n)
+		{
+			case 1:
+				a = value;
+				R1 = a * Math.Sqrt(3) / 6;
+				R2 = 2 * R1;
+				S = a * a * Math.Sqrt(3) / 4;
+				break;
+			case 2:
+				R1 = value;
+				a = 3 * R1 / Math.Sqrt(2);
+				R2 = 2 * R1;
+				S = a * a * Math.Sqrt(3) / 4;
+				break;
+			case 3:
+				R2 = value;
+				R1 = R2 / 2;
+				a = 3 * R1 / Math.Sqrt(2);
+				S = a * a * Math.Sqrt(3) / 4;
+				break;
+			case 4:
+				S = value;
+				a = Math.Sqrt(S * 4 / Math.Sqrt(3));
+				R1 = a * Math.Sqrt(3) / 6;
+				R2 = 2 * R1;
+				break;
+			default:
+				break;
+		}
+
+		return $"a = {Math.Round(a, 2)}, R1 = {Math.Round(R1, 2)}, R2 = {Math.Round(R2, 2)}, S = {Math.Round(S, 2)}";
+	}
+
+	public static string Case15(int n, int m)
+	{
+		var mast = n switch
+		{
+			1 => "пик",
+			2 => "треф",
+			3 => "бубен",
+			4 => "червей",
+			_ => String.Empty
+		};
+
+		var card = m switch
+		{
+			1 => "туз",
+			2 => "2",
+			3 => "3",
+			4 => "4",
+			5 => "5",
+			6 => "6",
+			7 => "7",
+			8 => "8",
+			9 => "9",
+			10 => "10",
+			11 => "валет",
+			12 => "дама",
+			13 => "король",
+			_ => String.Empty
+		};
+
+		return $"{card} {mast}";
+	}
+
+	public static string Case16(int n)
+	{
+		if (n < 20 || n > 69)
+		{
+			return "Неверный год";
+		}
+
+		string[] ones = {"", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
+		string[] tens = {"двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят"};
+
+		var resultBuilder = new StringBuilder(tens[(n - 20) / 10]);
+		if (n % 10 != 0)
+		{
+			resultBuilder.Append(" " + ones[n % 10]);
+		}
+
+		var lastDigit = n % 10;
+		resultBuilder.Append(lastDigit switch
+		{
+			1 => " год",
+			>= 2 and <= 4 => " года",
+			_ => " лет"
+		});
+
+		return resultBuilder.ToString ();
+	}
+
+	public static string Case17(int n)
+	{
+		string amount = n switch
+		{
+			1 => "одно",
+			2 => "два",
+			3 => "три",
+			4 => "четыре",
+			5 => "пять",
+			6 => "шесть",
+			7 => "семь",
+			8 => "восемь",
+			9 => "девять",
+			10 => "десять",
+			11 => "одиннадцать",
+			12 => "двенадцать",
+			13 => "тринадцать",
+			14 => "четырнадцать",
+			15 => "пятнадцать",
+			16 => "шестнадцать",
+			17 => "семнадцать",
+			18 => "восемнадцать",
+			19 => "девятнадцать",
+			20 => "двадцать",
+			21 => "двадцать одно",
+			22 => "двадцать два",
+			23 => "двадцать три",
+			24 => "двадцать четыре",
+			25 => "двадцать пять",
+			26 => "двадцать шесть",
+			27 => "двадцать семь",
+			28 => "двадцать восемь",
+			29 => "двадцать девять",
+			30 => "тридцать",
+			31 => "тридцать одно",
+			32 => "тридцать два",
+			33 => "тридцать три",
+			34 => "тридцать четыре",
+			35 => "тридцать пять",
+			36 => "тридцать шесть",
+			37 => "тридцать семь",
+			38 => "тридцать восемь",
+			39 => "тридцать девять",
+			40 => "сорок",
+			_ => "Invalid number"
+		};
+
+		string result = amount + " ";
+
+		var lastDigit = n % 10;
+
+		result += lastDigit switch
+		{
+			0 => "учебных задание",
+			1 => "учебное задание",
+			>= 2 and <= 4 => "учебных задания",
+			_ => "учебных заданий"
+		};
+
+		return result;
+	}
+
+	public static string Case18(int n)
+	{
+		if (n < 100 || n > 999)
+		{
+			return "Неверное число";
+		}
+
+		string[] ones = {"", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
+		string[] tens =
+		{
+			"десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят",
+			"девяносто"
+		};
+		string[] hundreds =
+			{"", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"};
+
+		string result = hundreds[n / 100];
+		if (n % 100 != 0)
+		{
+			result += " " + tens[(n % 100) / 10 - 1];
+		}
+
+		if (n % 10 != 0)
+		{
+			result += " " + ones[n % 10];
+		}
+
+		return result.Trim ();
+	}
+
+	public static string Case19(int n)
+	{
+		string result = "год ";
+
+		result += (n % 10) switch
+		{
+			0 or 1 => "бел",
+			2 or 3 => "черн",
+			4 or 5 => "зелен",
+			6 or 7 => "красн",
+			8 or 9 => "жёлт",
+			_ => ""
+		};
+
+		result += ((n + 1) % 5) switch
+		{
+			0 or 1 or 2 or 3 or 4 or 5 or 9 or 10 or 11 => "ой ",
+			6 or 7 or 8 => "ого ",
+			_ => ""
+		};
+
+		result += ((n + 8) % 12) switch
+		{
+			0 => "крысы",
+			1 => "коровы",
+			2 => "лошади",
+			3 => "овцы",
+			4 => "обезьяны",
+			5 => "змеи",
+			6 => "тигра",
+			7 => "зайца",
+			8 => "дракона",
+			9 => "курицы",
+			10 => "собаки",
+			11 => "свиньи",
+			_ => ""
+		};
+
+		return result;
+	}
+
+
+	public static string Case20(int m, int d)
+	{
+		string result = "";
+
+		result = m switch
+		{
+			1 => (d > 19) ? "Водолей" : "Козерог",
+			2 => (d > 18) ? "Рыбы" : "Водолей",
+			3 => (d > 20) ? "Овен" : "Рыбы",
+			4 => (d > 19) ? "Телец" : "Овен",
+			5 => (d > 20) ? "Близнецы" : "Телец",
+			6 => (d > 21) ? "Рак" : "Близнецы",
+			7 => (d > 22) ? "Лев" : "Рак",
+			8 => (d > 22) ? "Дева" : "Лев",
+			9 => (d > 22) ? "Весы" : "Дева",
+			10 => (d > 22) ? "Скорпион" : "Весы",
+			11 => (d > 22) ? "Стрелец" : "Скорпион",
+			12 => (d > 21) ? "Козерог" : "Стрелец",
+			_ => "Invalid month"
+		};
+
+		return result;
+	}
+}
---
+++ Tests/Case.cs
@@ -0,0 +1,252 @@
+﻿namespace Tests;
+
+[TestFixture]
+public class Case
+{
+	[TestCase(1, "Понедельник")]
+	[TestCase(2, "Вторник")]
+	[TestCase(3, "Среда")]
+	[TestCase(4, "Четверг")]
+	[TestCase(5, "Пятница")]
+	[TestCase(6, "Суббота")]
+	[TestCase(7, "Воскресенье")]
+	[TestCase(8, "Нет такого дня недели")]
+	public void Case1(int day, string expectedDayName)
+	{
+		Assert.That(TasksLesson.Homework.Case.Case1(day), Is.EqualTo(expectedDayName));
+	}
+
+	[TestCase(1, "Плохо")]
+	[TestCase(2, "Неудовлетворительно")]
+	[TestCase(3, "Удовлетворительно")]
+	[TestCase(4, "Хорошо")]
+	[TestCase(5, "Отлично")]
+	[TestCase(10, "Нет такой оценки")]
+	public void Case2(int grade, string expectedGradeDescription)
+	{
+		Assert.That(TasksLesson.Homework.Case.Case2(grade), Is.EqualTo(expectedGradeDescription));
+	}
+
+	[TestCase(1, "Январь")]
+	[TestCase(6, "Июнь")]
+	[TestCase(13, "Нет такого месяца")]
+	public void Case3(int month, string expectedMonthName)
+	{
+		Assert.That(TasksLesson.Homework.Case.Case3(month), Is.EqualTo(expectedMonthName));
+	}
+
+	[TestCase(1, "31")]
+	[TestCase(2, "28")]
+	[TestCase(3, "31")]
+	[TestCase(4, "30")]
+	[TestCase(12, "31")]
+	[TestCase(13, "Нет такого месяца")]
+	public void Case4(int month, string expectedDays)
+	{
+		Assert.That(TasksLesson.Homework.Case.Case4(month), Is.EqualTo(expectedDays));
+	}
+
+	[TestCase(3, 2, 1, ExpectedResult = "5")]
+	[TestCase(3, 2, 2, ExpectedResult = "1")]
+	[TestCase(3, 2, 3, ExpectedResult = "6")]
+	[TestCase(4, 2, 4, ExpectedResult = "2")]
+	[TestCase(3, 2, 5, ExpectedResult = "Нет такого действия")]
+	[TestCase(3, 0, 4, ExpectedResult = "Нельзя делить на ноль")]
+	public string Case5(int a, int b, int c)
+	{
+		return TasksLesson.Homework.Case.Case5(a, b, c);
+	}
+
+	[TestCase(1, 10, 1)]
+	[TestCase(2, 0.001, 1)]
+	[TestCase(3, 123, 123)]
+	[TestCase(4, 1000, 1)]
+	[TestCase(5, 100, 1)]
+	public void Case6_Correct(int a, double b, double expected)
+	{
+		double? result = TasksLesson.Homework.Case.Case6(a, b);
+		Assert.That(result, Is.EqualTo(expected));
+	}
+
+	[Test]
+	public void Case6_ShouldReturnNull_WhenInvalidCaseProvided()
+	{
+		// Act
+		double? result = TasksLesson.Homework.Case.Case6(0, 123);
+
+		// Assert
+		Assert.IsNull(result);
+	}
+
+	[TestCase(1, 100, ExpectedResult = 100.0)]
+	[TestCase(2, 1000, ExpectedResult = 0.001)]
+	[TestCase(3, 100, ExpectedResult = 0.1)]
+	[TestCase(4, 100, ExpectedResult = 100000)]
+	[TestCase(5, 100, ExpectedResult = 10000)]
+	[TestCase(6, 100, ExpectedResult = -1)]
+	public double Case7(int a, double b)
+	{
+		return TasksLesson.Homework.Case.Case7(a, b);
+	}
+
+	[TestCase(1, 2, ExpectedResult = "31 12")]
+	[TestCase(2, 2, ExpectedResult = "31 1")]
+	[TestCase(3, 2, ExpectedResult = "28 2")]
+	[TestCase(4, 2, ExpectedResult = "31 3")]
+	[TestCase(5, 2, ExpectedResult = "30 4")]
+	[TestCase(6, 2, ExpectedResult = "31 5")]
+	// Add more test cases as needed
+	public string Case8(int a, int b)
+	{
+		return TasksLesson.Homework.Case.Case8(a, b);
+	}
+
+	[TestCase(1, 1, ExpectedResult = "2 1")]
+	[TestCase(2, 2, ExpectedResult = "3 2")]
+	[TestCase(3, 3, ExpectedResult = "4 3")]
+	[TestCase(4, 4, ExpectedResult = "5 4")]
+	[TestCase(5, 5, ExpectedResult = "6 5")]
+	[TestCase(6, 6, ExpectedResult = "7 6")]
+	[TestCase(7, 7, ExpectedResult = "8 7")]
+	[TestCase(8, 8, ExpectedResult = "9 8")]
+	[TestCase(9, 9, ExpectedResult = "10 9")]
+	[TestCase(10, 10, ExpectedResult = "11 10")]
+	[TestCase(11, 11, ExpectedResult = "12 11")]
+	[TestCase(12, 12, ExpectedResult = "13 12")]
+	[TestCase(13, 13, ExpectedResult = "Нет такого месяца")]
+	public string Case9(int d, int m)
+	{
+		return TasksLesson.Homework.Case.Case9(d, m);
+	}
+
+	[TestCase('С', 0, ExpectedResult = "С")]
+	[TestCase('С', 1, ExpectedResult = "З")]
+	[TestCase('С', -1, ExpectedResult = "В")]
+	[TestCase('С', 100, ExpectedResult = "Нет такой команды")]
+	[TestCase('З', 0, ExpectedResult = "З")]
+	[TestCase('З', 1, ExpectedResult = "Ю")]
+	[TestCase('З', -1, ExpectedResult = "С")]
+	[TestCase('З', 100, ExpectedResult = "Нет такой команды")]
+	[TestCase('Ю', 0, ExpectedResult = "Ю")]
+	[TestCase('Ю', 1, ExpectedResult = "В")]
+	[TestCase('Ю', -1, ExpectedResult = "З")]
+	[TestCase('Ю', 100, ExpectedResult = "Нет такой команды")]
+	[TestCase('В', 0, ExpectedResult = "В")]
+	[TestCase('В', 1, ExpectedResult = "С")]
+	[TestCase('В', -1, ExpectedResult = "Ю")]
+	[TestCase('В', 100, ExpectedResult = "Нет такой команды")]
+	[TestCase('X', 0, ExpectedResult = "Нет такого направления")]
+	public string Case10(char a, int b)
+	{
+		return TasksLesson.Homework.Case.Case10(a, b);
+	}
+
+	[TestCase('С', 1, -1, ExpectedResult = 'С')]
+	[TestCase('З', -1, 2, ExpectedResult = 'Ю')]
+	public char Case11(char a, int b, int c)
+	{
+		return TasksLesson.Homework.Case.Case11(a, b, c);
+	}
+
+	[TestCase(1, 1, ExpectedResult = "R = 1, D = 2, L = 6,3, S = 3,1")]
+	[TestCase(2, 2, ExpectedResult = "R = 1, D = 2, L = 6,3, S = 3,1")]
+	[TestCase(3, 6.2832, ExpectedResult = "R = 1, D = 2, L = 6,3, S = 3,1")]
+	[TestCase(4, 3.1416, ExpectedResult = "R = 1, D = 2, L = 6,3, S = 3,1")]
+	[TestCase(5, 1, ExpectedResult = "Incorrect input")]
+	public string Case12(int a, double b)
+	{
+		return TasksLesson.Homework.Case.Case12(a, b);
+	}
+
+	[TestCase(1, 1.0, ExpectedResult = "a = 1, c = 1,41, h = 0,71, S = 0,35")]
+	[TestCase(2, 1.0, ExpectedResult = "a = 0,71, c = 1, h = 0,5, S = 0,25")]
+	[TestCase(3, 1.0, ExpectedResult = "a = 1,41, c = 2, h = 1, S = 1")]
+	[TestCase(4, 1.0, ExpectedResult = "a = 1, c = 1,41, h = 0,71, S = 1")]
+	[TestCase(5, 1.0, ExpectedResult = "a = 0, c = 0, h = 0, S = 0")]
+	public string Case13(int n, double value)
+	{
+		return TasksLesson.Homework.Case.Case13(n, value);
+	}
+
+	[TestCase(1, 100, ExpectedResult = "a = 100, R1 = 28,87, R2 = 57,74, S = 4330,13")]
+	[TestCase(2, 100, ExpectedResult = "a = 212,13, R1 = 100, R2 = 200, S = 19485,57")]
+	public string Case14(int n, double value)
+	{
+		return TasksLesson.Homework.Case.Case14(n, value);
+	}
+
+	[TestCase(1, 1, ExpectedResult = "туз пик")]
+	[TestCase(2, 2, ExpectedResult = "2 треф")]
+	[TestCase(3, 3, ExpectedResult = "3 бубен")]
+	[TestCase(4, 4, ExpectedResult = "4 червей")]
+	public string Case15(int suit, int value)
+	{
+		return TasksLesson.Homework.Case.Case15(suit, value);
+	}
+
+	[TestCase(1, ExpectedResult = "Неверный год")]
+	[TestCase(2, ExpectedResult = "Неверный год")]
+	[TestCase(20, ExpectedResult = "двадцать лет")]
+	[TestCase(30, ExpectedResult = "тридцать лет")]
+	[TestCase(35, ExpectedResult = "тридцать пять лет")]
+	[TestCase(41, ExpectedResult = "сорок один год")]
+	[TestCase(46, ExpectedResult = "сорок шесть лет")]
+	[TestCase(69, ExpectedResult = "шестьдесят девять лет")]
+	[TestCase(70, ExpectedResult = "Неверный год")]
+	public string Case16(int n)
+	{
+		return TasksLesson.Homework.Case.Case16(n);
+	}
+
+	[TestCase(1, ExpectedResult = "одно учебное задание")]
+	[TestCase(4, ExpectedResult = "четыре учебных задания")]
+	[TestCase(6, ExpectedResult = "шесть учебных заданий")]
+	[TestCase(21, ExpectedResult = "двадцать одно учебное задание")]
+	[TestCase(45, ExpectedResult = "Invalid number учебных заданий")]
+	public string Case17(int n)
+	{
+		var result = TasksLesson.Homework.Case.Case17(n);
+		return result;
+	}
+
+	[TestCase(123, "сто двадцать три")]
+	[TestCase(567, "пятьсот шестьдесят семь")]
+	[TestCase(999, "девятьсот девяносто девять")]
+	[TestCase(100, "сто")]
+	[TestCase(50, "Неверное число")]
+	public void Case18(int input, string expectedResult)
+	{
+		string result = TasksLesson.Homework.Case.Case18(input);
+
+		Assert.That(result, Is.EqualTo(expectedResult));
+	}
+
+	[TestCase(1972, ExpectedResult = "год черной крысы")]
+	[TestCase(1925, ExpectedResult = "год зеленой коровы")]
+	[TestCase(1935, ExpectedResult = "год зеленой свиньи")]
+	[TestCase(1924, ExpectedResult = "год зеленой крысы")]
+	[TestCase(2043, ExpectedResult = "год черной свиньи")]
+	public string Case19(int a)
+	{
+		return TasksLesson.Homework.Case.Case19(a);
+	}
+
+	[TestCase(1, 19, ExpectedResult = "Козерог")]
+	[TestCase(1, 20, ExpectedResult = "Водолей")]
+	[TestCase(2, 18, ExpectedResult = "Водолей")]
+	[TestCase(2, 19, ExpectedResult = "Рыбы")]
+	[TestCase(3, 20, ExpectedResult = "Рыбы")]
+	[TestCase(3, 21, ExpectedResult = "Овен")]
+	[TestCase(4, 19, ExpectedResult = "Овен")]
+	[TestCase(4, 20, ExpectedResult = "Телец")]
+	[TestCase(5, 20, ExpectedResult = "Телец")]
+	[TestCase(6, 21, ExpectedResult = "Близнецы")]
+	[TestCase(7, 22, ExpectedResult = "Рак")]
+	[TestCase(8, 23, ExpectedResult = "Дева")]
+	[TestCase(11, 22, ExpectedResult = "Скорпион")]
+	[TestCase(12, 21, ExpectedResult = "Стрелец")]
+	public string Case20(int month, int day)
+	{
+		return TasksLesson.Homework.Case.Case20(month, day);
+	}
+}
