7f7cb88444b15fc7287f75772949774318ada205
Tymur Slynko <timurslinko1@gmail.com>
2023-09-22 11:49:49 +0300

    "Add new Series and While class to the TasksLesson.Homework module"
    This commit introduces a new Series class and a While class to the Homework module within the TasksLesson namespace. Each of the classes brings numerous static methods which are intended to cover a wide array of mathematical functions for different use cases. For instance, Series class calculates different types of series while While class includes methods employing while loop for calculations. This is done to expand the module's functionality and provide users with more useful mathematical utilities.

---
+++ TasksLesson/Homework/Series.cs
@@ -0,0 +1,640 @@
+﻿namespace TasksLesson.Homework;
+
+public class Series
+{
+	public static void Series1(int[] numbers)
+	{
+		int sum = 0;
+		for (int i = 0; i < numbers.Length; i++)
+		{
+			sum += numbers[i];
+		}
+
+		Console.WriteLine(sum);
+	}
+
+	public static void Series2(float[] numbers)
+	{
+		float sum = 0;
+		for (int i = 0; i < numbers.Length; i++)
+		{
+			sum *= numbers[i];
+		}
+
+		Console.WriteLine(sum);
+	}
+
+	public static void Series3(int[] numbers)
+	{
+		int sum = 0;
+		for (int i = 0; i < numbers.Length; i++)
+		{
+			sum += numbers[i];
+		}
+
+		Console.WriteLine(sum / numbers.Length);
+	}
+
+	public static void Series4(int n, int[] numbers)
+	{
+		double sum = 0;
+		double pro = 1;
+
+		for (int i = 1; i <= n; i++)
+		{
+			sum += numbers[i];
+			pro *= numbers[i];
+		}
+
+		Console.WriteLine("Сумма: {0}", sum);
+		Console.WriteLine("Произведение: {0}", pro);
+	}
+
+	public static void Series5(int N, float[] numbers)
+	{
+		float sum = 0;
+		for (int i = 0; i < N; i++)
+		{
+			numbers[i] = MathF.Round(numbers[i], 0);
+			sum += numbers[i];
+			Console.WriteLine(numbers[i]);
+		}
+
+		Console.WriteLine(sum);
+	}
+
+	public static void Series6(int N, float[] numbers)
+	{
+		float sum = 0;
+		for (int i = 0; i < N; i++)
+		{
+			numbers[i] -= MathF.Round(numbers[i], 0);
+			sum *= numbers[i];
+			Console.WriteLine(numbers[i]);
+		}
+
+		Console.WriteLine(sum);
+	}
+
+	public static void Series7(int N, float[] numbers)
+	{
+		int sum = 0;
+		for (int i = 0; i < N; i++)
+		{
+			numbers[i] = MathF.Round(numbers[i], 0);
+			sum += (int) numbers[i];
+			Console.WriteLine(numbers[i]);
+		}
+
+		Console.WriteLine(sum);
+	}
+
+	public static void Series8(int N, int[] numbers)
+	{
+		int K = 0;
+		for (int i = 0; i < N; i++)
+		{
+			if (numbers[i].IsEven ())
+			{
+				K++;
+				Console.WriteLine(numbers[i]);
+			}
+		}
+
+		Console.WriteLine(K);
+	}
+
+	public static void Series9(int N, int[] numbers)
+	{
+		int K = 0;
+		for (int i = 0; i < N; i++)
+		{
+			if (numbers[i].IsOdd ())
+			{
+				K++;
+				Console.WriteLine(numbers[i]);
+			}
+		}
+
+		Console.WriteLine(K);
+	}
+
+	public static void Series10(int N, int[] numbers)
+	{
+		bool positive = false;
+		for (int i = 0; i < N; i++)
+		{
+			if (numbers[i].IsPositive ())
+			{
+				positive = true;
+			}
+		}
+
+		Console.WriteLine(positive);
+	}
+
+	public static void Series11(int n, int k, int[] numbers)
+	{
+		bool status = false;
+		for (int i = n; i <= n; i++)
+		{
+			if (numbers[i] < k)
+			{
+				status = true;
+			}
+		}
+
+		Console.WriteLine(status);
+	}
+
+	public static void Series12(int[] numbers)
+	{
+		for (int i = 0; i < numbers.Length; i++)
+		{
+			if (numbers[i] == 0)
+			{
+				Console.WriteLine(i);
+				break;
+			}
+		}
+	}
+
+	public static void Series13(int[] numbers)
+	{
+		int sum = 0;
+		for (int i = 0; i < numbers.Length; i++)
+		{
+			if (numbers[i] == 0)
+			{
+				Console.WriteLine(i);
+				break;
+			}
+			else if (numbers[i].IsPositive () && numbers[i].IsEven ())
+			{
+				sum += numbers[i];
+			}
+		}
+
+		Console.WriteLine(sum);
+	}
+
+	public static void Series14(int[] numbers, int k)
+	{
+		int count = 0;
+		for (int i = 0; i < numbers.Length; i++)
+		{
+			if (numbers[i] == 0)
+			{
+				Console.WriteLine(i);
+				break;
+			}
+			else if (numbers[i] < k)
+			{
+				count++;
+			}
+		}
+
+		Console.WriteLine(count);
+	}
+
+	public static void Series15(int[] numbers, int k)
+	{
+		int count = 0;
+		for (int i = 0; i < numbers.Length; i++)
+		{
+			if (numbers[i] == 0)
+			{
+				Console.WriteLine(i);
+				break;
+			}
+			else if (numbers[i] > k)
+			{
+				count = i;
+				break;
+			}
+		}
+
+		Console.WriteLine(count);
+	}
+
+	public static void Series16(int[] numbers, int k)
+	{
+		int count = 0;
+		for (int i = 0; i < numbers.Length; i++)
+		{
+			if (numbers[i] == 0)
+			{
+				Console.WriteLine(i);
+				break;
+			}
+			else if (numbers[i] > k)
+			{
+				count = i;
+			}
+		}
+
+		Console.WriteLine(count);
+	}
+
+	public static void Series17(float[] numbers, float b, float n)
+	{
+		bool isAlreadyWritten = true;
+
+		for (int i = 1; i <= n; i++)
+		{
+			if ((b <= numbers[i]) && isAlreadyWritten)
+			{
+				Console.WriteLine(b);
+				isAlreadyWritten = false;
+			}
+
+			Console.WriteLine(numbers[i]);
+		}
+
+		if (isAlreadyWritten)
+		{
+			Console.WriteLine(b);
+		}
+	}
+
+	public static void Series18(int[] numbers, int n)
+	{
+		var uniqueNumbers = new List<int> ();
+		for (int i = 0; i < n; i++)
+		{
+			if (!uniqueNumbers.Contains(numbers[i]))
+			{
+				uniqueNumbers.Add(numbers[i]);
+			}
+		}
+
+		foreach(var number in uniqueNumbers)
+		{
+			Console.WriteLine(number);
+		}
+	}
+
+	public static void Series19(int[] numbers, int n)
+	{
+		int count = 0;
+		for (int i = 1; i < n; i++)
+		{
+			if (numbers[i] < numbers[i - 1])
+			{
+				Console.WriteLine(numbers[i]);
+				count++;
+			}
+		}
+
+		Console.WriteLine(count);
+	}
+
+	public static void Series20(int[] numbers, int n)
+	{
+		int count = 0;
+		for (int i = 0; i < n - 1; i++)
+		{
+			if (numbers[i] < numbers[i + 1])
+			{
+				Console.WriteLine(numbers[i]);
+				count++;
+			}
+		}
+
+		Console.WriteLine(count);
+	}
+
+	public static void Series21(int n, float[] numbers)
+	{
+		bool status = false;
+		for (int i = 0; i < n - 1; i++)
+		{
+			if (numbers[i] < numbers[i + 1])
+			{
+				status = true;
+			}
+			else
+			{
+				status = false;
+				break;
+			}
+		}
+
+		Console.WriteLine(status);
+	}
+
+	public static void Series22(int n, float[] numbers)
+	{
+		for (int i = 0; i < n - 1; i++)
+		{
+			if (numbers[i] > numbers[i + 1])
+			{
+				Console.WriteLine(i + 1);
+				break;
+			}
+			else if (numbers[i] < numbers[i + 1])
+			{
+				Console.WriteLine(0);
+				break;
+			}
+		}
+	}
+
+	public static void Series23(int n, float[] numbers)
+	{
+		for (int i = 1; i < n - 1; i++)
+		{
+			bool isPeak = numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1];
+			bool isTrough = numbers[i] < numbers[i - 1] && numbers[i] < numbers[i + 1];
+			bool isIncreasing = numbers[i] > numbers[i - 1] && numbers[i] < numbers[i + 1];
+			bool isDecreasing = numbers[i] < numbers[i - 1] && numbers[i] > numbers[i + 1];
+
+			if (isPeak || isTrough)
+			{
+				Console.WriteLine(i);
+				break;
+			}
+			else if (isIncreasing || isDecreasing)
+			{
+				Console.WriteLine(0);
+				break;
+			}
+		}
+	}
+
+	public static void Series24(int n, int[] numbers)
+	{
+		int sum = 0;
+		int tempsum = 0;
+
+		for (int i = 1; i <= n; i++)
+		{
+			tempsum += numbers[i];
+			if (numbers[i] == 0)
+			{
+				sum = tempsum;
+				tempsum = 0;
+			}
+		}
+
+		Console.WriteLine(sum);
+	}
+
+	public static void Series25(int n, int[] numbers)
+	{
+		int sum = 0;
+		int tempsum = 0;
+		int f = 0;
+
+		for (int i = 1; i <= n; ++i)
+		{
+			tempsum += numbers[i];
+			if (numbers[i] == 0)
+			{
+				if (f != 0)
+				{
+					sum += tempsum;
+				}
+				else
+				{
+					f = 1;
+				}
+
+				tempsum = 0;
+			}
+		}
+	}
+
+	public static void Series26(int n, int k, float[] numbers)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			float result = 1;
+			for (int j = 0; j < k; j++)
+			{
+				result *= numbers[i];
+			}
+
+			Console.WriteLine(result);
+		}
+	}
+
+	public static void Series27(int n, float[] numbers)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			float result = 1;
+			for (int j = 1; j < i; j++)
+			{
+				result *= numbers[i];
+			}
+
+			Console.WriteLine(result);
+		}
+	}
+
+	public static void Series28(int n, float[] numbers)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			float result = 1;
+			for (int j = 1; j < n - i + 1; j++)
+			{
+				result *= numbers[i];
+			}
+
+			Console.WriteLine(result);
+		}
+	}
+
+	public static void Series29(int n, int k, int[] numbers)
+	{
+		int sum = 0;
+		for (int i = 0; i < n * k; i++)
+		{
+			sum += numbers[i];
+		}
+
+		Console.WriteLine(sum);
+	}
+
+	public static void Series30(int n, int k, int[] numbers)
+	{
+		for (int i = 0; i < n * k; i++)
+		{
+			int sum = 0;
+			for (int j = 0; j < n; j++)
+			{
+				sum += numbers[i];
+			}
+
+			Console.WriteLine(sum);
+		}
+	}
+
+	public static void Series31(int n, int k, int kount)
+	{
+		int count = 0;
+		for (int i = 0; i < n * k; i++)
+		{
+			if (kount == 2)
+			{
+				count++;
+			}
+		}
+
+		Console.WriteLine(count);
+	}
+
+	public static void Series32(int n, int k, int[] numbers)
+	{
+		for (int i = 0; i < n * k; i++)
+		{
+			if (numbers[i] == 2)
+			{
+				Console.WriteLine(i);
+				break;
+			}
+			else if (numbers[i] != 2)
+			{
+				Console.WriteLine(0);
+				break;
+			}
+		}
+	}
+
+	public static void Series33(int n, int k, int[] numbers)
+	{
+		for (int i = 0; i < n * k; i++)
+		{
+			if (numbers[i] == 2)
+			{
+				Console.WriteLine(i);
+				break;
+			}
+			else if (numbers[i] != 2)
+			{
+				Console.WriteLine(0);
+				break;
+			}
+		}
+	}
+
+	public static void Series34(int n, int k, int[] numbers)
+	{
+		for (int i = 0; i < n * k; i++)
+		{
+			if (numbers[i] == 2)
+			{
+				int sum = 0;
+				for (int j = 0; j < n; j++)
+				{
+					sum += numbers[i];
+				}
+
+				Console.WriteLine(sum);
+				break;
+			}
+			else if (numbers[i] != 2)
+			{
+				Console.WriteLine(0);
+				break;
+			}
+		}
+	}
+
+	public static void Series35(int n, int k, int[] numbers)
+	{
+		int count = 0;
+		for (int i = 0; i < n * k; i++)
+		{
+			if (numbers[i] == 0)
+			{
+				count++;
+			}
+		}
+
+		Console.WriteLine(count);
+	}
+
+	public static void Series36(int n, int k, int[] numbers)
+	{
+		int count = 0;
+		for (int i = 0; i < n * k; i++)
+		{
+			if (numbers[i] < numbers[i + 1])
+			{
+				count++;
+			}
+		}
+
+		Console.WriteLine(count);
+	}
+
+	public static void Series37(int n, int k, int[] numbers)
+	{
+		int count = 0;
+		for (int i = 0; i < n * k; i++)
+		{
+			if (numbers[i] < numbers[i + 1])
+			{
+				count++;
+			}
+		}
+
+		Console.WriteLine(count);
+	}
+
+	public static void Series38(int n, int k, int[] numbers)
+	{
+		for (int i = 0; i < n * k; i++)
+		{
+			if (numbers[i] < numbers[i + 1])
+			{
+				Console.WriteLine(1);
+				break;
+			}
+			else if (numbers[i] > numbers[i + 1])
+			{
+				Console.WriteLine(-1);
+				break;
+			}
+			else if (numbers[i] == numbers[i + 1])
+			{
+				Console.WriteLine(0);
+				break;
+			}
+		}
+	}
+
+	public static void Series39(int n, int k, int[] numbers)
+	{
+		int count = 0;
+		for (int i = 0; i < n * k; i++)
+		{
+			if (numbers[i] < numbers[i + 1])
+			{
+				count++;
+			}
+		}
+
+		Console.WriteLine(count);
+	}
+
+	public static void Series40(int n, int k, int[] numbers)
+	{
+		for (int i = 0; i < n * k; i++)
+		{
+			if ((numbers[i] < numbers[i + 1] && numbers[i + 1] > numbers[i + 2]) ||
+			    (numbers[i] > numbers[i + 1] && numbers[i + 1] < numbers[i + 2]) ||
+			    (numbers[i] == numbers[i + 1] && numbers[i + 1] == numbers[i + 2]))
+			{
+				Console.WriteLine(numbers[i]);
+			}
+			else
+			{
+				Console.WriteLine(0);
+			}
+		}
+	}
+}
---
+++ TasksLesson/Homework/While.cs
@@ -0,0 +1,440 @@
+﻿namespace TasksLesson.Homework;
+
+public class While
+{
+	public static void While1(int a, int b)
+    {
+        while (a >= b)
+        {
+            a -= b;
+        }
+        Console.WriteLine(a);
+    }
+
+    public static void While2(int a, int b)
+    {
+        int count = 0;
+        while (a >= b)
+        {
+            a -= b;
+            count++;
+        }
+        Console.WriteLine(count);
+    }
+
+    public static void While3(uint n, uint k)
+    {
+        int num = 0;
+        while (n >= k) {
+            n -= k;
+            num++;
+        }
+        Console.WriteLine(num);
+    }
+
+    public static void While4(uint a)
+    {
+        while (a > 0) a -= 3;
+
+        if (a == 0)
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+    }
+
+    public static void While5(uint n)
+    {
+        int k = 0;
+
+        while (n > 1)
+        {
+            n /= 2;
+            k++;
+        }
+
+        if (n == 1)
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+    }
+
+    public static void While6(int n)
+    {
+        float result = 1;
+
+        while (n >= 2)
+        {
+            result *= n;
+            n -= 2;
+        }
+
+        Console.WriteLine(result);
+    }
+
+    public static void While7(int n)
+    {
+        int k = 1;
+
+        while (k * k <= n)
+            k++;
+
+        Console.WriteLine(k);
+    }
+
+    public static void While8(int n)
+    {
+        int k = 1;
+
+        while (k * k <= n)
+            k++;
+
+        Console.WriteLine(k - 1);
+    }
+
+    public static void While9(int n)
+    {
+        int k = 1;
+        int temp = 3;
+
+        while (temp <= n)
+        {
+            temp *= 3;
+            k++;
+        }
+
+        Console.WriteLine(k);
+    }
+
+    public static void While10(int n)
+    {
+        int k = 0;
+        int temp = 3;
+
+        while (temp <= n)
+        {
+            temp *= 3;
+            k++;
+        }
+
+        Console.WriteLine(k);
+    }
+
+    public static void While11(int n)
+    {
+        int k = 0;
+        int sum = 0;
+
+        while (sum < n)
+        {
+            k++;
+            sum += k;
+        }
+
+        Console.WriteLine(k);
+        Console.WriteLine(sum);
+    }
+
+    public static void While12(int n)
+    {
+        int sum = 0;
+        int k = 0;
+
+        while (sum <= n)
+        {
+            sum += k + 1;
+            k++;
+        }
+
+        sum -= k + 1;
+        Console.WriteLine(k - 1);
+        Console.WriteLine(sum);
+    }
+
+    public static void While13(int a)
+    {
+        double k = 1;
+        double sum = 0;
+
+        while (sum < a)
+        {
+            sum += 1.0 / k;
+            k++;
+        }
+        Console.WriteLine(k);
+        Console.WriteLine(sum);
+    }
+
+    public static void While14(int a)
+    {
+        double sum = 0;
+        int k = 0;
+
+        while(sum < a)
+        {
+            k++;
+            sum += (double) 1 / k;
+        }
+        Console.WriteLine("Число K становится равным {0}, а сумма равна {1}", k, sum);
+    }
+
+    public static void While15(int k)
+    {
+        double s = 1000;
+        double p = 10;
+
+        while (s <= 1100)
+        {
+            k++;
+            s += s * p / 100;
+        }
+
+        Console.WriteLine($"K = {k}, S = {s}");
+    }
+
+    public static void While16(int p)
+    {
+        int k = 1;
+        double s = 10;
+        double sum = 10;
+
+        while (sum <= 200)
+        {
+            k++;
+            s += s * p / 100;
+            sum += s;
+        }
+
+        Console.WriteLine($"K = {k}, S = {sum}");
+    }
+
+    public static void While17(int n)
+    {
+        while (n > 0)
+        {
+            Console.Write(n % 10);
+            n /= 10;
+        }
+    }
+
+    public static void While18(int n)
+    {
+        int sum = 0;
+        int count = 0;
+
+        while (n > 0)
+        {
+            sum += n % 10;
+            n /= 10;
+            count++;
+        }
+
+        Console.WriteLine($"Количество цифр: {count}");
+        Console.WriteLine($"Сумма цифр: {sum}");
+    }
+
+
+    public static void While19(int n)
+    {
+        int result = 0;
+        int tenMult = 1;
+
+        while (n > 0)
+        {
+            int rest = n % 10;
+            result += tenMult * rest;
+            n /= 10;
+            tenMult *= 10;
+        }
+        Console.WriteLine(result);
+    }
+
+    public static void While20(int n)
+    {
+        bool isTwoInNumber = false;
+
+        while (n > 0)
+        {
+            int reminder = n % 10;
+            if (reminder == 2)
+            {
+                isTwoInNumber = true;
+                break;
+            }
+
+            n /= 10;
+        }
+        Console.WriteLine(isTwoInNumber);
+    }
+
+    public static void While21(int n)
+    {
+        bool isOdd = false;
+
+        while (n > 0)
+        {
+            int reminder = n % 10;
+            if (reminder % 2 != 0)
+            {
+                isOdd = true;
+                break;
+            }
+
+            n /= 10;
+        }
+        Console.WriteLine(isOdd);
+    }
+
+    public static void While22(int n)
+    {
+        bool isPrime = true;
+
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        Console.WriteLine(isPrime);
+    }
+
+    public static void While23(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        Console.WriteLine(a);
+    }
+
+    public static void While24(int n)
+    {
+        int f1 = 1;
+        int f2 = 1;
+        int f3 = 0;
+        bool isFibonacci = false;
+
+        while (f3 < n)
+        {
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+            if (f3 == n)
+            {
+                isFibonacci = true;
+                break;
+            }
+        }
+        Console.WriteLine(isFibonacci);
+    }
+
+    public static void While25(int n)
+    {
+        int f1 = 1;
+        int f2 = 1;
+        int f3 = 0;
+
+        while (f3 <= n)
+        {
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+        }
+        Console.WriteLine(f3);
+    }
+
+    public static void While26(int n)
+    {
+        int f1 = 1;
+        int f2 = 1;
+        int f3 = 0;
+
+        while (f3 < n)
+        {
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+        }
+        Console.WriteLine($"Fk-1 = {f1}");
+        Console.WriteLine($"Fk+1 = {f3}");
+    }
+
+    public static void While27(int n)
+    {
+        int f1 = 1;
+        int f2 = 1;
+        int f3 = 0;
+        int k = 2;
+
+        while (f3 < n)
+        {
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+            k++;
+        }
+        Console.WriteLine(k);
+    }
+
+    public static void While28(double e)
+    {
+        double a1 = 2;
+        double a2 = 2 + 1 / a1;
+        int k = 2;
+
+        while (Math.Abs(a2 - a1) >= e)
+        {
+            a1 = a2;
+            a2 = 2 + 1 / a1;
+            k++;
+        }
+        Console.WriteLine($"K = {k}");
+        Console.WriteLine($"A1 = {a1}");
+        Console.WriteLine($"A2 = {a2}");
+    }
+
+    public static void While29(double e)
+    {
+        double a1 = 1;
+        double a2 = 2;
+        double a3 = (a1 + 2 * a2) / 3;
+        int k = 3;
+
+        while (Math.Abs(a3 - a2) >= e)
+        {
+            a1 = a2;
+            a2 = a3;
+            a3 = (a1 + 2 * a2) / 3;
+            k++;
+        }
+        Console.WriteLine($"K = {k}");
+        Console.WriteLine($"A1 = {a1}");
+        Console.WriteLine($"A2 = {a2}");
+        Console.WriteLine($"A3 = {a3}");
+    }
+    public static void While30(int a, int b, int c)
+    {
+        int count = 0;
+        int a1 = a;
+        int b1 = b;
+
+        while (a1 >= c && b1 >= c)
+        {
+            a1 -= c;
+            b1 -= c;
+            count++;
+        }
+        Console.WriteLine(count);
+    }
+}
